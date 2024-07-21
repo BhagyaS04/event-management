83d5ca29af9fb04ef15b54064faaa3a46df8fbc0
BhagyaS04 <bhagyasuchith@gmail.com>
2024-07-21 22:55:05 +0530

    fetching from db successfull

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://<username>:<password>@cluster0.d05tceu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then((res)=>{
+mongoose.connect('mongodb+srv://<username>:<password>@cluster0.d05tceu.mongodb.net/eventdb?retryWrites=true&w=majority&appName=Cluster0').then((res)=>{
     console.log('DB is connected')
 }).catch((res)=>{
     console.log('DB is not connected')
